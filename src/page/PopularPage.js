94c60f293c4df0dfdae3ff58bfb0ffa83f40cb03
kyr1eee <781246808@qq.com>
2019-03-24 02:58:21 +0800

    动态顶部Tab

--- src/page/PopularPage.js
+++ src/page/PopularPage.js
@@ -3,21 +3,40 @@ import {StyleSheet, Text, View} from 'react-native';
 import {createMaterialTopTabNavigator} from 'react-navigation';
 import PopularTab from '../components/PopularTab';
 export default class PopularPage extends Component{
+  constructor(props) {
+    super(props);
+    this.tabNames = ['JavaScript', 'React', 'Vue', 'TypeScript', 'React Native', 'Java']
+  }
+
+  generateTab() {
+      const tabs = {};
+      this.tabNames.forEach((item, index) => {
+        tabs[`tab${index}`] = {
+            // 切换Tab传递参数进组件
+            screen: props => <PopularTab {...this.props} tabLabel={item}/>,
+            navigationOptions: {
+                title: item,
+            },
+        }
+      });
+      return tabs;
+  }
+
   render() {
-    const TabNavigator = createMaterialTopTabNavigator({
-        PopularTab1: {
-            screen: PopularTab,
-            navigationOptions: {
-                title: 'Tab1'
+    const TabNavigator = createMaterialTopTabNavigator(
+        this.generateTab(), {
+            tabBarOptions: {
+                tabStyle: styles.tabStyle,
+                upperCaseLabel: false,  // 是否标签为大写, 默认true
+                scrollEnabled: true,    // 是否支持选项卡滚动, 默认false
+                style: {
+                    backgroundColor: '#ea6f5a'  // 背景颜色
+                },
+                indicatorStyle: styles.indicatorStyle,  // 标签指示器样式
+                labelStyle: styles.labelStyle,  // 文字样式
             }
-        },
-        PopularTab2: {
-            screen: PopularTab,
-            navigationOptions: {
-                title: 'Tab2'
-            }
-        },
-    });
+        }
+    );
     return (
         <View style={styles.container}>
             <TabNavigator />
@@ -31,9 +50,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabStyle: {
+      minWidth: 50
+  },
+  indicatorStyle: {
+      height: 2,
+      backgroundColor: 'white'
+  },
+  labelStyle: {
+    fontSize: 16
   }
 });
